8bf063a3153584c062dfacd838678e055d32718b
tanishka-dahiya <55584381+tanishka-dahiya@users.noreply.github.com>
2020-01-16 22:21:22 +0530

    jwt token

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const { check, validationResult } = require("express-validator/check");
 const User = require("../../models/User");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 //@route POSTapi/users
 //@desc Register User
 //@access Public
@@ -56,7 +58,22 @@ router.post(
       await user.save();
       //Return jsonwebtoken
 
-      res.send("user Registered");
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err);
       res.status(500).send("Server error");
