b08ab502a968fb7d2ad1a5f01839dd623dd6bf5b
tanishka-dahiya <55584381+tanishka-dahiya@users.noreply.github.com>
2020-01-21 16:34:19 +0530

    added empty store

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,24 @@ import Register from "./components/layout/auth/Register";
 import Login from "./components/layout/auth/Login";
 
 import "./App.css";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
