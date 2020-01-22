588c9d07c9565efeaef6f8fe12fb67006791d17f
tanishka-dahiya <55584381+tanishka-dahiya@users.noreply.github.com>
2020-01-22 13:26:02 +0530

    changes for deployment

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const connectDB = require("./config/db");
 var cors = require("cors");
 const app = express();
+const path = require("path");
 //connect Database
 connectDB();
 //Init Middleware
 app.use(cors());
 app.use(express.json({ extended: false }));
-app.get("/", (req, res) => res.send("api running"));
 
 //Define Routes
 
@@ -17,6 +17,14 @@ app.use("/api/auth", require("./routes/api/auth"));
 
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+//serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started ${PORT}`));
